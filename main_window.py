4fce5775ad556010642955914c2bb9d4546673fa
Huỳnh Khánh Duy <huykhaduy@gmail.com>
2024-04-02 23:42:12 +0700

    3. tạo 1 video player đơn giản

--- main_window.py
+++ main_window.py
@@ -1,31 +1,71 @@
 from PyQt5 import uic
-from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget
-from qfluentwidgets import PushButton, TitleLabel, setTheme, Theme
+from PyQt5.QtCore import QUrl
+from PyQt5.QtMultimedia import QMediaPlayer, QMediaContent
+from PyQt5.QtMultimediaWidgets import QVideoWidget
+from PyQt5.QtWidgets import QApplication, QLabel, QVBoxLayout, QWidget, QGraphicsView, QSlider, QStyle
+from qfluentwidgets import PushButton
 
-# Tài liệu chapter 2: Cách sài GUI Designer
-# Download gui designer: https://build-system.fman.io/qt-designer-download
-# Video hướng dẫn: https://youtu.be/Fk1TBoBcrR4?t=2494
-
-# --- NÂNG CAO SỬ DỤNG qfluentwidgets trong GUI DESIGNER ---
-# Hướng dẫn link https://qfluentwidgets.com/pages/designer/#promoting-widget
+# 3. Tạo màn hình video với pyqt5
+# Tài liệu: https://stackoverflow.com/questions/57842104/how-to-play-videos-in-pyqt
 
 class MainWindow(QWidget):
     def __init__(self):
         super().__init__()
         uic.loadUi("designer.ui", self)
 
-        self.count = 0
+        # Lấy các widget từ ui
+        self.videoWidget = self.findChild(QVideoWidget, "widget")
+        self.playButton = self.findChild(PushButton, "playButton")
 
-        # Tìm thằng button có objectName là button1 trong designer.ui
-        self.button = self.findChild(PushButton, "button1") # Trong video là pushButton nhưng ở đây là button1
-        # Liên kết hàm onClick với sự kiện click của button
-        self.button.clicked.connect(self.onClick)
+        self.nextButton = self.findChild(PushButton, "nextButton")
+        self.prevButton = self.findChild(PushButton, "prevButton")
+        self.slider = self.findChild(QSlider, "slider")
 
-        # Tìm thằng label có objectName là label1 trong designer.ui
-        self.label = self.findChild(QLabel, "label1")
+        self.playButton.clicked.connect(self.play)
 
-    def onClick(self):
-        self.count += 1
-        self.label.setText(f"Counter: {self.count}")
+        # Media player là một class dùng để play video, không có giao diện
+        self.mediaPlayer = QMediaPlayer(None, QMediaPlayer.VideoSurface)
+        # Set link video
+        self.mediaPlayer.setMedia(QMediaContent(
+            QUrl("https://web.lotuscdn.vn/2024/1/8/89a826e1b25444ed6901435f92388d26_1704675063636-vcsi47dfxd.mp4/720.m3u8")))
+        self.mediaPlayer.setVideoOutput(self.videoWidget)
+        self.mediaPlayer.stateChanged.connect(self.mediaStateChanged)
+        self.mediaPlayer.positionChanged.connect(self.positionChanged)
+        self.mediaPlayer.durationChanged.connect(self.durationChanged)
+        self.mediaPlayer.error.connect(self.handleError)
+
+        self.slider.sliderMoved.connect(self.changeSiderValue)
+
+    def play(self):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.mediaPlayer.pause()
+        else:
+            self.mediaPlayer.play()
+
+    def changeSiderValue(self):
+        print("Sider change"+str(self.slider.value()))
+        self.mediaPlayer.setPosition(self.slider.value())
+
+    def mediaStateChanged(self, state):
+        if self.mediaPlayer.state() == QMediaPlayer.PlayingState:
+            self.playButton.setIcon(
+                self.style().standardIcon(QStyle.SP_MediaPause))
+        else:
+            self.playButton.setIcon(
+                self.style().standardIcon(QStyle.SP_MediaPlay))
+
+    def positionChanged(self, position):
+        self.slider.setValue(position)
+
+    def durationChanged(self, duration):
+        self.slider.setRange(0, duration)
+
+    def setPosition(self, position):
+        self.mediaPlayer.setPosition(position)
+
+    def handleError(self):
+        self.playButton.setEnabled(False)
+
+
 
 
